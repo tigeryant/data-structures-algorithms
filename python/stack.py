0d2506041a32395b8cb17f570775d4fef131bdf5
John Davies <john.davies@jreddington.ltd.uk>
2022-05-09 09:10:00 +0100

    stack implemented in python

---
+++ python/stack.py
@@ -0,0 +1,56 @@
+from contextlib import nullcontext
+
+
+class Stack:
+    def __init__(self):
+        self.top = -1
+        self.stack = []
+
+    def is_empty(self):
+        if self.top == -1:
+            print('The stack is empty.')
+            return True
+        else:
+            print('The stack is not empty.')
+            return False
+
+    def size(self):
+        size = self.top + 1
+        print(f'The size of the stack is {size} elements.')
+
+    def topElement(self):
+        if self.is_empty():
+            print('There is no top element in the stack.')
+        else:
+            top_element = self.stack[self.top]
+            print(f'The top element is: {top_element}.')
+    
+    def push(self, element):
+        self.top = self.top + 1
+        self.stack.insert(self.top, element)
+        print(f'{element} was added to the stack.')
+
+    def pop(self):
+        if self.is_empty():
+            print('Nothing to pop - the stack is empty.')
+        else:
+            element = self.stack[self.top]
+            self.top = self.top - 1
+            print(f'{element} was removed from the stack.')
+
+myStack = Stack()
+
+myStack.size()
+myStack.topElement()
+
+myStack.push(10)
+myStack.push(20)
+myStack.push(30)
+
+myStack.size()
+myStack.topElement()
+
+myStack.pop()
+myStack.size()
+myStack.topElement()
+    
